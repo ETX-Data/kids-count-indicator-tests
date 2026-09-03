46f263ca44c87d8bdad5ab1316eb4d9cecbdf04c
Kaitlan Wong <kaitlan.wong@gmail.com>
2026-09-03 13:09:43 -0500

    added comments

--- valid_data_formats.py
+++ valid_data_formats.py
@@ -1,3 +1,5 @@
 #### add other valid data formats as you go in the indicator cleaning process
+#### for example, I'm pretty sure some indicators use Rate Per 1,000
+#### see the current Kids Count Data Guide to learn about each indicator (internal document, ask Kaitlan if you can't find it)
 
 valid_data_formats = {"Rate Per 100,000", "Number", "Percent"}
